7c02a57da522440bc47d24969aeb0566fc6ca193
tytu16 <ttuschhoff11@gmail.com>
2017-05-30 19:40:19 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ assignments = []
 
 # Cross_Product Function
 def cross(A, B):
-    "Cross product of elements in A and elements in B."
+    # Cross product of elements in A and elements in B.
     return [a+b for a in A for b in B]
 
 rows = 'ABCDEFGHI'
